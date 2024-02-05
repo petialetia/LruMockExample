6f2326ac9155b524d07d527d95bb29557b0278e2
petialetia <petialetia@yandex.ru>
2024-02-05 19:31:55 +0300

    google tests installation is added

--- requirements_ubuntu.sh
+++ requirements_ubuntu.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+START_FOLDER=$(pwd)
+
 apt-get update
 apt-get install -y sudo
 
 sudo apt-get install -y clang
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y cmake
 
+sudo apt-get install -y libgtest-dev
+cd /usr/src/gtest
+sudo cmake .
+sudo make
+cd "$START_FOLDER"
