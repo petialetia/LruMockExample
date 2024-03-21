df4e9674196f657dbfe16dd8424bc72185a48bbd
petialetia <petialetia@yandex.ru>
2024-03-21 08:30:33 +0300

    mock tests are written

---
+++ tests/FibonacciTests.cpp
@@ -0,0 +1,30 @@
+//TODO: Change to your own tests
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <Fibonacci.hpp>
+
+#include <boost/optional.hpp>
+#include <boost/optional/optional_io.hpp>
+
+template<typename Key, typename Value>
+class MockLruCache
+{
+public:
+    MOCK_METHOD(boost::optional<Value>, get, (const Key& key), ());
+    MOCK_METHOD(void, insert, (const Key& key, const Value& value), ());
+};
+
+TEST(Fibonacci, MockCheckGet)
+{
+    MockLruCache<std::size_t, std::size_t> lru_storage;
+
+    EXPECT_CALL(lru_storage, get(0))
+        .WillOnce(testing::Return(boost::none));
+
+    EXPECT_CALL(lru_storage, insert(0, 0));
+
+    auto answer = evalFibonacciCaching(0, lru_storage);
+    EXPECT_EQ(answer, 0);
+}
