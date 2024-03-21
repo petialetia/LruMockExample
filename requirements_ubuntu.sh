306cae2ee364067fa2ff3353fdb5184e6025e836
petialetia <petialetia@yandex.ru>
2024-03-21 08:45:44 +0300

    gtests are removed from requirements installation

--- requirements_ubuntu.sh
+++ requirements_ubuntu.sh
@@ -8,9 +8,3 @@ apt-get install -y sudo
 sudo apt-get install -y clang
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y cmake
 sudo apt-get install -y libboost-all-dev
-
-sudo apt-get install -y libgtest-dev
-cd /usr/src/gtest
-sudo cmake .
-sudo make
-cd "$START_FOLDER"
